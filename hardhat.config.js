6c585329d601ebb710999a5bf9df2b7e5eab73f4
Anay Verma <anayverma2147@gmail.com>
2024-09-02 15:04:16 +0530

    Blockchain-1

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,43 @@
-require("@nomicfoundation/hardhat-toolbox");
-
 /** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config({path:'./.env.local'});
+require("@nomiclabs/hardhat-waffle");
+
+task("accounts", "Print all the accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// module.exports = {
+//   solidity: "0.8.24",
+// };
+
 module.exports = {
-  solidity: "0.8.24",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.NEXT_PUBLIC_RPC_URL,
+      accounts: [process.env.NEXT_PUBLIC_PRIVATE_KEY]
+    },
+  },
+  solidity: {
+    version: "0.8.10",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000,
+  },
 };
